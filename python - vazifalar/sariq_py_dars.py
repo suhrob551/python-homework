2b1ddfa3f732924ea6d169ac3d69a317a46538c6
suhrob551 <80281754+suhrob551@users.noreply.github.com>
2022-06-20 21:12:19 +0900

    yangi fayllar qoshildi

---
+++ python - vazifalar/sariq_py_dars.py
@@ -0,0 +1,61 @@
+# menu = ["a", "s", "d", "f", "g", "h"]
+# buyurtmalar = []
+# for i in range(3):
+#     buyurtmalar.append(input())
+#
+# if buyurtmalar:
+#     for buyurtma in buyurtmalar:
+#         if buyurtma in menu:
+#             print(f"{buyurtma} menuda bor")
+#         else:
+#             print(f"{buyurtma} menuda yoq")
+# else:
+#     print("hali buyurtma bermagansiz")
+
+# mahsulotlar = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
+# savat = []
+# for i in range(5):
+#     savat.append(input(f"{i + 1} - mah.: "))
+#
+# for sav in savat:
+#     if sav in mahsulotlar:
+#         print(f"dokonda {sav} bor")
+#     else:
+#         print(f"dokonda {sav} yoq")
+
+# mahsulotlar = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
+# savat = []
+# for i in range(5):
+#     savat.append(input(f"{i + 1} - mah/: "))
+#
+# mavjud_mahsulotlar = []
+# mavjud_emas = []
+#
+# for s in savat:
+#     if s in mahsulotlar:
+#         mavjud_mahsulotlar.append(s)
+#     else:
+#         mavjud_emas.append(s)
+#
+# if mavjud_emas:
+#     print("Mavjud emaslar quyidagilar: ")
+#     for n in mavjud_emas:
+#         print(n)
+# else:
+#     print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+
+# users = ["qw", "we", "er", "rt", "ty"]
+# new_login = input("login kiriting\n>>>")
+#
+# if new_login.lower() in users:
+#     print("login band")
+# else:
+#     print(f"xush kelibsiz, {new_login}!")
+
+# n = int(input())
+#
+# for i in range(2, 10):
+#     if n % i == 0:
+#         print(f"{n} soni {i} ga qoldiqsiz bolinadi")
+
+
